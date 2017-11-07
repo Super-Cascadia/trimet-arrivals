fc1d09339815815df455c0072dba32148c75ada8
Jameson Nyeholt <jameson.nyeholt@gmail.com>
2017-11-06 23:25:07 -0800

    add time until arrival indicator

--- src/view/arrivals/components/ArrivalRow.tsx
+++ src/view/arrivals/components/ArrivalRow.tsx
@@ -14,13 +14,22 @@ function getDistanceUntilArrival(feet: number): number {
     return feet && feet < MILE ? MILE / feet : feet && feet / MILE;
 }
 
-class ArrivalRow extends React.Component<Props> {    
+class ArrivalRow extends React.Component<Props> {
+    timeToArrivalIndicator(seconds: number) {
+        if (seconds === 0) {
+            return <span className="arrival-on-time"> On time</span>;
+        } else {
+            return <span className="arrival-estimated-late">{seconds}s late</span>
+        }
+    }
+
     render() {
         const { arrival } = this.props;
         const scheduled = moment(arrival.scheduled);
-        const estimated = moment(arrival.scheduled);
+        const estimated = moment(arrival.estimated);
         const seconds = moment(scheduled.diff(estimated)).seconds();
-        
+        const untilArrival = moment(estimated.diff(moment.now())).seconds();
+
         const scheduledTime = scheduled.format('ddd, h:mm:ss a');
         const estimatedTime = estimated.format('ddd, h:mm:ss a');         
         const distance = getDistanceUntilArrival(arrival.feet);
@@ -31,8 +40,9 @@ class ArrivalRow extends React.Component<Props> {
                     <RouteIndicator route={arrival.route} />
                 </td>
                 <td>{arrival.shortSign}</td>
+                <td>{this.timeToArrivalIndicator(seconds)}</td>
+                <td>{untilArrival}s away</td>
                 <td>{Math.round(distance)} miles</td>
-                <td>{seconds}</td>
                 <td>{estimatedTime}</td>   
                 <td>{scheduledTime}</td>                     
             </tr>
