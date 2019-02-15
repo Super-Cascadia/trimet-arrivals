c2003396b5db014105197244922ae858f478f2b2
wintermuted <jameson.nyeholt@gmail.com>
2019-02-15 13:40:44 -0800

    issues/5: add tests for LateIndicator

--- src/view/arrivals/components/LateIndicator.tsx
+++ src/view/arrivals/components/LateIndicator.tsx
@@ -8,21 +8,24 @@ interface Props {
     estimated: Moment;
 }
 
-class LateIndicator extends React.PureComponent<Props> {
+export default class LateIndicator extends React.PureComponent<Props> {
     render() {
         const { scheduled, estimated } = this.props;
-        const diff = scheduled.diff(estimated);
-        const seconds = moment(diff).seconds();
-        const minutes = moment(diff).minutes();
-        const minutesDiff = 59 - minutes;
-        const secondsDiff = 60 - seconds;
 
-        if (minutesDiff === 0) {
-            return <span className="arrival-estimated-late">{secondsDiff}s late</span>;
+        if (scheduled && estimated) {
+            const diff = scheduled.diff(estimated);
+            const seconds = moment(diff).seconds();
+            const minutes = moment(diff).minutes();
+            const minutesDiff = 59 - minutes;
+            const secondsDiff = 60 - seconds;
+
+            if (minutesDiff === 0) {
+                return <span className="arrival-estimated-late">{secondsDiff}s late</span>;
+            }
+
+            return <span className="arrival-estimated-late">{minutesDiff}m {secondsDiff}s late</span>;
+        } else {
+            return <span className="arrival-estimated-late">-</span>
         }
-
-        return <span className="arrival-estimated-late">{minutesDiff}m {secondsDiff}s late</span>;
     }
 }
-
-export default LateIndicator;
