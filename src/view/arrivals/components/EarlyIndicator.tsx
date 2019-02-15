e69b9ca705278b2f19d0d0635fc004ea01d23658
wintermuted <jameson.nyeholt@gmail.com>
2019-02-15 13:49:56 -0800

    issues/30: clean up EarlyIndicator test

--- src/view/arrivals/components/EarlyIndicator.tsx
+++ src/view/arrivals/components/EarlyIndicator.tsx
@@ -8,22 +8,25 @@ interface Props {
     estimated: Moment;
 }
 
-class EarlyIndicator extends React.PureComponent<Props> {
+export default class EarlyIndicator extends React.PureComponent<Props> {
     render() {
         const { scheduled, estimated } = this.props;
-        const diff = estimated.diff(scheduled);
-        const seconds = moment(diff).seconds();
-        const minutes = moment(diff).minutes();
 
-        const minutesDiff = 59 - minutes;
-        const secondsDiff = 60 - seconds;
+        if (scheduled && estimated) {
+            const diff = estimated.diff(scheduled);
+            const seconds = moment(diff).seconds();
+            const minutes = moment(diff).minutes();
 
-        if (minutesDiff === 0) {
-            return <span className="arrival-estimated-early">{secondsDiff}s early</span>;
+            const minutesDiff = 59 - minutes;
+            const secondsDiff = 60 - seconds;
+
+            if (minutesDiff === 0) {
+                return <span className="arrival-estimated-early">{secondsDiff}s early</span>;
+            }
+
+            return <span className="arrival-estimated-early">{59 - minutes}m {60 - seconds}s early</span>;
         }
 
-        return <span className="arrival-estimated-early">{59 - minutes}m {60 - seconds}s early</span>;
+        return "-"
     }
 }
-
-export default EarlyIndicator;
