bf6f9a009674ae9eb68389ad580d74883adfd42c
wintermuted <jameson.nyeholt@gmail.com>
2019-02-16 14:05:12 -0800

    issues/30: WIP: stop component test

---
+++ src/view/stops/StopComponent.test.tsx
@@ -0,0 +1,130 @@
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import StopComponent from './StopComponent';
+import { ProviderMock } from '../../test/util';
+import { Arrival, StopLocation } from '../../api/trimet/types';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+function mockStore() {
+    const initialState = {
+        loading: false,
+        timeOfLastLoad: ''
+    };
+
+    const arrivalsReducer = (state = initialState) => {
+        return state;
+    };
+
+    const baseState = {
+        arrivalsReducer: {
+            loading: {
+                123: false
+            },
+            arrivals: {
+                123: {} as Arrival
+            }
+        }
+    };
+
+    return createStore(combineReducers({arrivalsReducer}), baseState, applyMiddleware(thunk));
+}
+
+describe('StopComponent', () => {
+    describe('by default', () => {
+        it('renders without crashing', () => {
+            expect(() => shallow(
+                <StopComponent
+                    stopLocation={undefined}
+                    loadArrivalData={undefined}
+                    locationId={undefined}
+                    loading={undefined}
+                    showArrivals={undefined}
+                />
+            )).not.toThrow();
+        });
+
+        it('shows a StopsTableHeader', function () {
+            const subject = shallow(
+                <StopComponent
+                    stopLocation={undefined}
+                    loadArrivalData={undefined}
+                    locationId={undefined}
+                    loading={undefined}
+                    showArrivals={undefined}
+                />);
+
+            expect(subject.find('StopsTableHeader')).toExist();
+        });
+
+        it('shows an ArrivalsContainer', function () {
+            const subject = mount(
+                <ProviderMock store={mockStore()}>
+                    <StopComponent
+                        stopLocation={undefined}
+                        loadArrivalData={undefined}
+                        locationId={undefined}
+                        loading={undefined}
+                        showArrivals={undefined}
+                    />
+                </ProviderMock>
+                );
+
+            const stop = subject.find('.stop');
+
+            expect(stop.childAt(1).name()).toBe('Connect(ArrivalsComponent)');
+        });
+
+        describe('when mounted', function () {
+            const loadArrivalDataSpy = jasmine.createSpy('loadArrivalDataSpy');
+            const stopLocation = { } as StopLocation;
+
+            const subject = mount(
+                <ProviderMock store={mockStore()}>
+                    <StopComponent
+                        stopLocation={stopLocation}
+                        loadArrivalData={loadArrivalDataSpy}
+                        locationId={123}
+                        loading={false}
+                        showArrivals={true}
+                    />
+                </ProviderMock>
+            );
+
+            it('calls the load arrival data delegate', function () {
+                expect(loadArrivalDataSpy).toHaveBeenCalled();
+            });
+
+            it('sets a refresh interval', function () {
+                const stopComponent = subject.find('StopComponent')
+
+                expect(stopComponent).toExist();
+                expect(stopComponent.instance().refreshInterval).toBe(2)
+            });
+
+            describe('and the refresh interval expires', function () {
+                it('calls loadArrivalData delegate again', function () {
+
+                });
+            });
+        });
+    });
+
+    describe('when mounted', function () {
+        describe('and a reload is triggered by user interaction', function () {
+
+        });
+
+        describe('when loadArrivals is called', function () {
+            describe('and a refresh interval exists', function () {
+                it('clears the interval', function () {
+
+                });
+
+                it('resets the refresh interval', function () {
+
+                });
+            });
+        });
+    });
+});
