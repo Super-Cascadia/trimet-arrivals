268f3b90ceab57d45b5745a345da36a5c69bbe0b
wintermuted <jameson.nyeholt@gmail.com>
2019-02-16 13:21:20 -0800

    issues/30: fix StopsComponent test name

--- src/view/stops/StopsComponent.test.tsx
+++ src/view/stops/StopsComponent.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import StopsComponent from './StopsComponent';
 import { StopLocation } from '../../api/trimet/types';
 
-describe('ArrivalsContainer', () => {
+describe('StopsComponent', () => {
     describe('by default', () => {
         it('renders without crashing', () => {
             expect(() => shallow(
