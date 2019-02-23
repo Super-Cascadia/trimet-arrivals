d0557d1ce917aba836770358ff8c43899fbaae9e
wintermuted <jameson.nyeholt@gmail.com>
2019-02-23 10:04:05 -0800

    feat(api-fixture): add default Portland location setting for geolocation clean up stopActions

--- src/api/geolocation/index.ts
+++ src/api/geolocation/index.ts
@@ -1,8 +1,17 @@
 import { Location } from "../trimet/types";
-import { fixtureEnabled } from "../util";
+import { fixtureEnabled, useDefaultPortlandLocation } from "../util";
 import { geoLocationFixtureData } from "./fixture";
 
-export function getCurrentPosition() {
+export function getCurrentPosition(): Promise<Location> {
+  if (useDefaultPortlandLocation()) {
+    return Promise.resolve({
+      coords: {
+        latitude: 45.5122,
+        longitude: -122.6587
+      }
+    });
+  }
+
   if (fixtureEnabled()) {
     return geoLocationFixtureData();
   }
