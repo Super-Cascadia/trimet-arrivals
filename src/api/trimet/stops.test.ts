32e4c49aa7d3b7cd5a732436c9afd9754444115a
wintermuted <jameson.nyeholt@gmail.com>
2019-02-24 21:28:18 -0800

    feat(api-fixture): add tests to stops API module to cover fixture data

--- src/api/trimet/stops.test.ts
+++ src/api/trimet/stops.test.ts
@@ -1,28 +1,54 @@
 import { getNearbyStops } from "./stops";
-import { getTrimetData } from "./util";
+import { StopData } from "./types";
 
 jest.mock("./util", () => {
   return {
     getTrimetData() {
-      return Promise.resolve({ foo: "bar" });
+      return Promise.resolve({
+        location: [{}],
+        queryTime: "123"
+      });
     }
   };
 });
 
-describe("stops", () => {
-  const location = {
-    coords: {
-      latitude: 123,
-      longitude: 123
-    }
-  };
+describe("getNearbyStops", () => {
+  afterEach(() => {
+    process.env.REACT_APP_USE_FIXTURE = undefined;
+  });
 
-  getNearbyStops(location, 123);
+  describe("when fixture data is enabled", () => {
+    it("retuns fixture data for stops", done => {
+      process.env.REACT_APP_USE_FIXTURE = "true";
 
-  it("foo", done => {
-    getTrimetData().then(result => {
-      expect(result).toEqual({ foo: "bar" });
-      done();
+      const location = {
+        coords: {
+          latitude: 123,
+          longitude: 123
+        }
+      };
+
+      getNearbyStops(location, 123).then((result: StopData) => {
+        expect(result.queryTime).toEqual("123");
+        done();
+      });
+    });
+  });
+
+  describe("when fixture data is not enabled", () => {
+    it("fetches trimet data from stops", done => {
+      const location = {
+        coords: {
+          latitude: 123,
+          longitude: 123
+        }
+      };
+
+      getNearbyStops(location, 123).then((result: StopData) => {
+        expect(result.location).toEqual([{}]);
+        expect(result.queryTime).toEqual("123");
+        done();
+      });
     });
   });
 });
