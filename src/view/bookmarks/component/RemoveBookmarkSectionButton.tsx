0e9856ef6592dc1fa817ac4a549eaa36fd169ba2
wintermuted <jameson.nyeholt@gmail.com>
2019-05-09 23:44:13 -0700

    feat(pin-stops): add confirm prompt for deleting bookmark section

--- src/view/bookmarks/component/RemoveBookmarkSectionButton.tsx
+++ src/view/bookmarks/component/RemoveBookmarkSectionButton.tsx
@@ -5,13 +5,19 @@ interface RemoveBookmarkSectionButtonProps {
   removeBookmarkSection: () => void;
 }
 
+function confirmDeletion(removeBookmarkSection) {
+  if (window.confirm("Are you sure you want to remove this section?")) {
+    removeBookmarkSection();
+  }
+}
+
 export const RemoveBookmarkSectionButton = ({
   removeBookmarkSection
 }: RemoveBookmarkSectionButtonProps) => {
   return (
     <button
       className="close-button group-menu-item"
-      onClick={removeBookmarkSection}
+      onClick={confirmDeletion.bind(this, removeBookmarkSection)}
       title="Remove Bookmark Section"
     >
       <FontAwesome name="times-circle" />
