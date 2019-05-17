68e24927346dcfb2717c73ac023e042535b360d5
wintermuted <jameson.nyeholt@gmail.com>
2019-05-17 10:18:14 -0700

    feat(pin-stops): add an additional BookmarksViewComponent test

--- src/view/bookmarks/component/BookmarksViewComponent.test.tsx
+++ src/view/bookmarks/component/BookmarksViewComponent.test.tsx
@@ -12,8 +12,24 @@ describe("BookmarksViewComponent", () => {
 
     it("shows a message about bookmarking a stop when none are present", () => {
       const subject = shallow(<BookmarksViewComponent bookmarks={undefined} />);
+      const noBookmarksMessage = subject.find(".no-bookmarks-container");
 
-      expect(subject.find(".no-bookmarks-container")).toExist();
+      expect(noBookmarksMessage).toExist();
+      expect(noBookmarksMessage.text()).toBe(
+        "Bookmark a stop and see it here."
+      );
+    });
+  });
+
+  describe("when provided bookmarks", () => {
+    it("shows a StopContainer", () => {
+      const bookmarks = [{}, {}];
+
+      const subject = shallow(<BookmarksViewComponent bookmarks={bookmarks} />);
+      const bookmarkStops = subject.find(".bookmark-stop-wrapper");
+
+      expect(bookmarkStops).toExist();
+      expect(bookmarkStops.children().length).toBe(2);
     });
   });
 });
