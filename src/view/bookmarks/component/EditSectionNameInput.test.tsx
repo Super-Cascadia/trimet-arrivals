4e5d9c759c2ff4c5a6aea8eb42b1a1be03c3bce4
wintermuted <jameson.nyeholt@gmail.com>
2019-05-17 11:04:39 -0700

    feat(pin-stops): add tests for EditSectionNameInput

--- src/view/bookmarks/component/EditSectionNameInput.test.tsx
+++ src/view/bookmarks/component/EditSectionNameInput.test.tsx
@@ -15,4 +15,31 @@ describe("EditSectionNameInput", () => {
       ).not.toThrow();
     });
   });
+
+  describe("when text is entered", () => {
+    const updateBookmarkSectionNameSpy = jasmine.createSpy(
+      "updateBookmarkSectionName"
+    );
+
+    const subject = shallow(
+      <EditSectionNameInput
+        sectionName={undefined}
+        updateBookmarkSectionName={updateBookmarkSectionNameSpy}
+      />
+    );
+
+    subject.simulate("change", {
+      target: {
+        value: "foo"
+      }
+    });
+
+    it("updates component state", () => {
+      expect(subject.state().sectionName).toBe("foo");
+    });
+
+    it("dispatches the updateBookmarkSectionName", () => {
+      expect(updateBookmarkSectionNameSpy).toHaveBeenCalledWith("foo");
+    });
+  });
 });
