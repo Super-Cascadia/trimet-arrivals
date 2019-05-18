604772b1a45776f198033e173645f80d96891283
wintermuted <jameson.nyeholt@gmail.com>
2019-05-18 10:36:21 -0400

    feat(pin-stops): add tests for bookmarksReducer

--- src/store/reducers/bookmarksReducer.test.ts
+++ src/store/reducers/bookmarksReducer.test.ts
@@ -1,13 +1,15 @@
-import { CREATE_STOP_BOOKMARK, REMOVE_STOP_BOOKMARK } from "../constants";
+import {
+  CREATE_STOP_BOOKMARK,
+  LOAD_BOOKMARKS_COMPLETE,
+  REMOVE_STOP_BOOKMARK
+} from "../constants";
 import bookmarksReducer from "./bookmarksReducer";
 
 describe("bookmarksReducer", () => {
   describe("when a bookmark is created", () => {
     const action = {
       payload: {
-        stopLocation: {
-          locid: 123
-        }
+        stopLocation: { locid: 123 }
       },
       type: CREATE_STOP_BOOKMARK
     };
@@ -21,9 +23,7 @@ describe("bookmarksReducer", () => {
 
   describe("when a bookmark is removed", () => {
     const action = {
-      payload: {
-        locationId: 123
-      },
+      payload: { locationId: 123 },
       type: REMOVE_STOP_BOOKMARK
     };
 
@@ -38,6 +38,23 @@ describe("bookmarksReducer", () => {
 
     it("removes existing bookmarks with a matching id", () => {
       expect(state.bookmarks).toEqual({ 456: { locid: 456 } });
+    });
+  });
+
+  describe("when loading bookmarks is complete", () => {
+    const action = {
+      payload: { bookmarks: { foo: "bar" } },
+      type: LOAD_BOOKMARKS_COMPLETE
+    };
+
+    const initialState = {
+      bookmarks: {}
+    };
+
+    const state = bookmarksReducer(initialState, action);
+
+    it("adds bookmarks to the store", () => {
+      expect(state.bookmarks).toEqual({ foo: "bar" });
     });
   });
 });
