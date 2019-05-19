fa0b3f89af8262261b7294a0e77aeb6003e54236
wintermuted <jameson.nyeholt@gmail.com>
2019-05-18 22:04:35 -0400

    feat(pin-stops): bring back arrivalSagas tests

--- src/store/sagas/arrivalSagas.test.ts
+++ src/store/sagas/arrivalSagas.test.ts
@@ -1,22 +1,39 @@
 // tslint:disable:no-submodule-imports
-import { put } from "redux-saga/effects";
+import { call, put } from "redux-saga/effects";
+import { getArrivals } from "../../api/trimet/arrivals";
 // tslint:enable:no-submodule-imports
-import { REMOVE_STOP_BOOKMARK } from "../constants";
-import { removeStopBookmark } from "./bookmarkSagas";
+import { LOAD_ARRIVALS, LOAD_ARRIVALS_COMPLETE } from "../constants";
+import { loadArrivalData } from "./arrivalSagas";
 
 describe("arrivalSagas", () => {
-  describe("removeStopBookmark", () => {
-    const removeStopBookmarkData = removeStopBookmark({
+  describe("loadArrivalData", () => {
+    const arrivalData = loadArrivalData({
       payload: { locationId: 123 }
     });
 
     it("dispatches the load arrivals event", () => {
-      expect(removeStopBookmarkData.next().value).toEqual(
+      expect(arrivalData.next().value).toEqual(
         put({
           payload: {
             locationId: 123
           },
-          type: REMOVE_STOP_BOOKMARK
+          type: LOAD_ARRIVALS
+        })
+      );
+    });
+
+    it("calls getArrivals api", () => {
+      expect(arrivalData.next().value).toEqual(call(getArrivals, "123", 45));
+    });
+
+    it("dispatches the load arrivals complete vent", () => {
+      expect(arrivalData.next({}).value).toEqual(
+        put({
+          payload: {
+            arrivalData: {},
+            locationId: 123
+          },
+          type: LOAD_ARRIVALS_COMPLETE
         })
       );
     });
