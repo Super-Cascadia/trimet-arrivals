f93b4c2405adf842224944f4711556e0b472f59f
wintermuted <jameson.nyeholt@gmail.com>
2019-05-19 11:30:34 -0400

    feat(pin-stops): switch App test to use mount

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
 import App from "./App";
 
 it("renders without crashing", () => {
-  expect(() => shallow(<App />)).not.toThrow();
+  expect(() => mount(<App />)).not.toThrow();
 });
