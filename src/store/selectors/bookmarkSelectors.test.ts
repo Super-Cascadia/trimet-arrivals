45e6b67202a0b98ff9c536bb2df7a3739eace5a2
wintermuted <jameson.nyeholt@gmail.com>
2019-05-25 21:12:53 -0400

    feat(pin-stops): add missing test for bookmarkSelectors

--- src/store/selectors/bookmarkSelectors.test.ts
+++ src/store/selectors/bookmarkSelectors.test.ts
@@ -1,6 +1,7 @@
 import { RootState } from "../reducers";
 import {
   bookmarkCountSelector,
+  bookmarkedStopLocationSelector,
   bookmarkedStopLocationsSelector,
   stopIsBookmarkedSelector
 } from "./bookmarkSelectors";
@@ -11,14 +12,37 @@ describe("bookmarkSelectors", () => {
       const state = {
         bookmarksReducer: {
           bookmarks: {
-            foo: "bar"
+            123: {
+              locid: 123
+            }
           }
         }
       };
 
       const result = bookmarkedStopLocationsSelector(state as RootState);
 
-      expect(result).toEqual({ foo: "bar" });
+      expect(result).toEqual({ 123: { locid: 123 } });
+    });
+  });
+
+  describe("bookmarkedStopLocationSelector", () => {
+    it("returns all bookmarks", () => {
+      const state = {
+        bookmarksReducer: {
+          bookmarks: {
+            123: {
+              locid: 123
+            },
+            234: {
+              locid: 234
+            }
+          }
+        }
+      };
+
+      const result = bookmarkedStopLocationSelector(state as RootState);
+
+      expect(result).toEqual([{ locid: 123 }, { locid: 234 }]);
     });
   });
 
