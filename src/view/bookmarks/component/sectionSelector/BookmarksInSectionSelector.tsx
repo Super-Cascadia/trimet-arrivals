7200e10416f4e042d1c3b5c74aafa0bf66826275
wintermuted <jameson.nyeholt@gmail.com>
2019-05-26 10:29:06 -0400

    feat(pin-stops): add tests for ReactSelect configuration methods

--- src/view/bookmarks/component/sectionSelector/BookmarksInSectionSelector.tsx
+++ src/view/bookmarks/component/sectionSelector/BookmarksInSectionSelector.tsx
@@ -1,64 +1,7 @@
-import chroma from "chroma-js";
-import { map } from "lodash";
 import React from "react";
 import Select from "react-select";
 import { StopLocation } from "../../../../api/trimet/types";
-
-function customStyles() {
-  return {
-    control: styles => ({ ...styles, backgroundColor: "white" }),
-    multiValue: (styles, { data }) => {
-      const color = chroma(data.color);
-      return {
-        ...styles,
-        backgroundColor: color.alpha(0.1).css()
-      };
-    },
-    multiValueLabel: (styles, { data }) => ({
-      ...styles,
-      color: data.color
-    }),
-    multiValueRemove: (styles, { data }) => ({
-      ...styles,
-      color: data.color,
-      hover: {
-        backgroundColor: data.color,
-        color: "white"
-      }
-    }),
-    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-      const color = chroma(data.color);
-      return {
-        ...styles,
-        backgroundColor: isDisabled
-          ? null
-          : isSelected
-          ? data.color
-          : isFocused
-          ? color.alpha(0.1).css()
-          : null,
-        color: isDisabled
-          ? "#ccc"
-          : isSelected
-          ? chroma.contrast(color, "white") > 2
-            ? "white"
-            : "black"
-          : data.color,
-        cursor: isDisabled ? "not-allowed" : "default"
-      };
-    }
-  };
-}
-
-function formatStopLocations(bookmarks: StopLocation[]) {
-  return map(bookmarks, stop => {
-    return {
-      color: "#FF5630",
-      label: `${stop.locid}: ${stop.desc}`,
-      value: stop.locid
-    };
-  });
-}
+import { customStyles, formatStopLocations } from "./sectionSelectorUtil";
 
 interface Props {
   bookmarksInSection: StopLocation[];
