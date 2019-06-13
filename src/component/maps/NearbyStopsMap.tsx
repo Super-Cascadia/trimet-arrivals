bd3428a7304538d00f2ab3141436f14c1c58db96
wintermuted <jameson.nyeholt@gmail.com>
2019-06-12 19:19:00 -0700

    feat(view-stops-in-map): break out mapbox setup methods to utils file

--- src/component/maps/NearbyStopsMap.tsx
+++ src/component/maps/NearbyStopsMap.tsx
@@ -1,13 +1,16 @@
-import { map } from "lodash";
 import mapboxgl from "mapbox-gl";
 import React, { Component } from "react";
-import { StopLocation } from "../../api/trimet/types";
+import data from "../../data/1/1_0.json";
 import { StopLocationsDictionary } from "../../store/reducers/stopsReducer";
+import {
+  setCurrentLocationMarker,
+  setNearbyStopMarkers
+} from "./util/mapboxUtils";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamFtZXNvbm55ZWhvbHQiLCJhIjoiY2p3NWoyamV0MTk1dDQ0cGNmdGZkenViMiJ9.TqDD3r62vlPzVgPnYjocsg";
 
-type LatLngCoords = number[];
+export type LatLngCoords = number[];
 
 interface Props {
   currentLocation: LatLngCoords;
@@ -15,107 +18,11 @@ interface Props {
 }
 
 export default class NearbyStopsMap extends Component<Props> {
-  private static setCurrentLocationMarker(
-    mapBoxMap,
-    currentLocation: LatLngCoords
-  ) {
-    if (currentLocation) {
-      mapBoxMap.addLayer({
-        id: "currentlocation",
-        layout: {
-          "icon-image": "rocket-15"
-        },
-        source: {
-          data: {
-            features: [
-              {
-                geometry: {
-                  coordinates: currentLocation,
-                  type: "Point"
-                },
-                properties: {},
-                type: "Feature"
-              }
-            ],
-            type: "FeatureCollection"
-          },
-          type: "geojson"
-        },
-        type: "symbol"
-      });
-    }
-  }
-
-  private static setLocations(stopLocations: StopLocationsDictionary) {
-    return map(stopLocations, (stopLocation: StopLocation) => {
-      return {
-        geometry: {
-          coordinates: [stopLocation.lng, stopLocation.lat],
-          type: "Point"
-        },
-        properties: {},
-        type: "Feature"
-      };
-    });
-  }
-
-  private static setNearbyStopMarkers(
-    mapBoxMap,
-    stopLocations: StopLocationsDictionary
-  ) {
-    mapBoxMap.addLayer({
-      id: "symbols",
-      layout: {
-        "icon-image": "bus-15"
-      },
-      source: {
-        data: {
-          features: NearbyStopsMap.setLocations(stopLocations),
-          type: "FeatureCollection"
-        },
-        type: "geojson"
-      },
-      type: "symbol"
-    });
-
-    // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
-    mapBoxMap.on("click", "symbols", e => {
-      mapBoxMap.flyTo({ center: e.features[0].geometry.coordinates });
-    });
-
-    // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
-    mapBoxMap.on("mouseenter", "symbols", () => {
-      mapBoxMap.getCanvas().style.cursor = "pointer";
-    });
-
-    // Change it back to a pointer when it leaves.
-    mapBoxMap.on("mouseleave", "symbols", () => {
-      mapBoxMap.getCanvas().style.cursor = "";
-    });
-  }
-
-  private static setMapFeatures(
-    mapBoxMap,
-    stopLocations: StopLocationsDictionary,
-    currentLocation: LatLngCoords
-  ) {
-    mapBoxMap.on("load", () => {
-      NearbyStopsMap.setNearbyStopMarkers(mapBoxMap, stopLocations);
-      NearbyStopsMap.setCurrentLocationMarker(mapBoxMap, currentLocation);
-    });
-  }
-
   private mapContainer: HTMLDivElement;
   private map: mapboxgl.Map;
 
   public componentDidMount() {
     this.initializeMap(this.props.currentLocation, this.props.stopLocations);
-
-    fetch("../../scripts/routes/1/1_0.json")
-      .then(response => response.json())
-      // tslint:disable
-      .then(data => console.log(data));
-    // tslint:enable
   }
 
   public componentWillUnmount() {
@@ -136,6 +43,10 @@ export default class NearbyStopsMap extends Component<Props> {
     currentLocation: LatLngCoords,
     stopLocations: StopLocationsDictionary
   ) {
+    // tslint:disable
+    console.log(data);
+    // tslint:enable
+
     this.map = new mapboxgl.Map({
       // @ts-ignore
       center: currentLocation,
@@ -144,6 +55,9 @@ export default class NearbyStopsMap extends Component<Props> {
       zoom: 15.25
     });
 
-    NearbyStopsMap.setMapFeatures(this.map, stopLocations, currentLocation);
+    this.map.on("load", () => {
+      setNearbyStopMarkers(this.map, stopLocations);
+      setCurrentLocationMarker(this.map, currentLocation);
+    });
   }
 }
