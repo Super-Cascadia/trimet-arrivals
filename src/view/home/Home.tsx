84399d744f136500f8758df1b775e0dca7510c84
wintermuted <jameson.nyeholt@gmail.com>
2022-04-05 21:14:58 -0700

    feat(homepage): wip alerts up

--- src/view/home/Home.tsx
+++ src/view/home/Home.tsx
@@ -1,13 +1,138 @@
+import moment from "moment";
 import React, { useEffect, useState } from "react";
 import {
+  Accordion,
+  Badge,
   Card,
   Col,
   Container,
   ListGroup,
   ListGroupItem,
-  Row
+  Row,
+  Table
 } from "react-bootstrap";
 import { getSytemAlerts } from "../../api/trimet/alerts";
+
+function getStopClosureTable(locations) {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Stop ID</th>
+          <th>Desc</th>
+          <th>Direction</th>
+        </tr>
+      </thead>
+      <tbody>
+        {locations.map(loc => {
+          return (
+            <tr key={loc.id}>
+              <td>{loc.id}</td>
+              <td>{loc.desc}</td>
+              <td>{loc.dir}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+function getRouteAlertTable(routes) {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Route ID</th>
+          <th>Desc</th>
+        </tr>
+      </thead>
+      <tbody>
+        {routes.map(location => {
+          return (
+            <tr key={location.id}>
+              <td>{location.id}</td>
+              <td>{location.desc}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+function getImpactedRoutes(routes) {
+  return routes.map(route => {
+    return (
+      <Badge bg="primary" key={route.id}>
+        {route.desc}
+      </Badge>
+    );
+  });
+}
+
+function getInfoCard() {
+  return (
+    <Card>
+      <Card.Header>Welcome to Trimet Arrivals</Card.Header>
+      <Card.Body>
+        Trimet Arrivals is a web service that can be used to get useful
+        information about the state of the Trimet Transit Service.
+      </Card.Body>
+    </Card>
+  );
+}
+
+function getSystemAlertsCard(systemAlertsData) {
+  return (
+    <Card>
+      <Card.Header>System Alerts</Card.Header>
+      <ListGroup className="list-group-flush">
+        {systemAlertsData &&
+          systemAlertsData.map(alert => {
+            return (
+              <ListGroupItem key={alert.id}>
+                <small>
+                  {alert.header_text} - {alert.desc}
+                </small>
+              </ListGroupItem>
+            );
+          })}
+      </ListGroup>
+    </Card>
+  );
+}
+
+function getAllAlertsCard(alertsData) {
+  return (
+    <Card>
+      <Card.Header>Route & Stop Alerts</Card.Header>
+
+      <Accordion defaultActiveKey="0">
+        {alertsData &&
+          alertsData.map(alert => {
+            return (
+              <Accordion.Item eventKey={alert.id} key={alert.id}>
+                <Accordion.Header>
+                  {alert.location && <Badge bg="warning">Stop Closure</Badge>}
+                  {alert.route && getImpactedRoutes(alert.route)}
+                  {alert.id} - {alert.header_text} -{" "}
+                  {moment(alert.begin).format("MM-DD-YY")} to{" "}
+                  {moment(alert.end).format("MM-DD-YY")}
+                </Accordion.Header>
+                <Accordion.Body>
+                  {alert.desc}
+                  <br />
+                  {alert.location && getStopClosureTable(alert.location)}
+                  {alert.route && getRouteAlertTable(alert.route)}
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+      </Accordion>
+    </Card>
+  );
+}
 
 function Home() {
   const [alertsData, setAlertsData] = useState(undefined);
@@ -24,7 +149,11 @@ function Home() {
         alert => alert.system_wide_flag !== true
       );
 
-      setAlertsData(nonSystemAlerts);
+      const nonSystemAlertsSorted = nonSystemAlerts.sort((a, b) => {
+        return b.begin - a.begin;
+      });
+
+      setAlertsData(nonSystemAlertsSorted);
       setSystemAlertsData(systemAlerts);
 
       // console.log(result);
@@ -35,42 +164,11 @@ function Home() {
     <Container>
       <br />
       <Row>
+        <Col>{getInfoCard()}</Col>
         <Col>
-          <Card>
-            <Card.Header>Welcome</Card.Header>
-            <Card.Body>Welcome</Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Header>Alerts</Card.Header>
-            <Card.Body>System Alerts</Card.Body>
-            <ListGroup className="list-group-flush">
-              {systemAlertsData &&
-                systemAlertsData.map(alert => {
-                  return (
-                    <ListGroupItem key={alert.id}>
-                      <small>
-                        {alert.header_text} - {alert.desc}
-                      </small>
-                    </ListGroupItem>
-                  );
-                })}
-            </ListGroup>
-            <Card.Body>Other Alerts</Card.Body>
-            <ListGroup className="list-group-flush">
-              {alertsData &&
-                alertsData.map(alert => {
-                  return (
-                    <ListGroupItem key={alert.id}>
-                      <small>
-                        {alert.header_text} - {alert.desc}
-                      </small>
-                    </ListGroupItem>
-                  );
-                })}
-            </ListGroup>
-          </Card>
+          {getSystemAlertsCard(systemAlertsData)}
+          <br />
+          {getAllAlertsCard(alertsData)}
         </Col>
       </Row>
     </Container>
