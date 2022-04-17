cfb478aafffcc9bdc06a83c5c9aa9384e6a7af0b
wintermuted <jameson.nyeholt@gmail.com>
2022-04-16 18:34:15 -0700

    feat(home): cleanup home alerts api processing

--- src/view/home/Home.tsx
+++ src/view/home/Home.tsx
@@ -1,34 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { getSytemAlerts } from "../../api/trimet/alerts";
+import { AlertsData } from "../../api/trimet/interfaces/alertsData";
 import AllAlertsCard from "./AllAlertsCard";
 import { InfoCard } from "./InfoCard";
 import { SystemAlertsCard } from "./SystemAlertsCard";
+
+function processAlertsData(
+  result: AlertsData,
+  setAlertsData: (value: unknown) => void,
+  setSystemAlertsData: (value: unknown) => void
+) {
+  const alerts = result.alert;
+
+  const systemAlerts = alerts.filter(alert => alert.system_wide_flag === true);
+  const nonSystemAlerts = alerts.filter(
+    alert => alert.system_wide_flag !== true
+  );
+
+  const nonSystemAlertsSorted = nonSystemAlerts.sort((a, b) => {
+    return b.begin - a.begin;
+  });
+
+  setAlertsData(nonSystemAlertsSorted);
+  setSystemAlertsData(systemAlerts);
+}
 
 function Home() {
   const [alertsData, setAlertsData] = useState(undefined);
   const [systemAlertsData, setSystemAlertsData] = useState(undefined);
 
   useEffect(() => {
-    getSytemAlerts().then(result => {
-      const alerts = result.alert;
-
-      const systemAlerts = alerts.filter(
-        alert => alert.system_wide_flag === true
-      );
-      const nonSystemAlerts = alerts.filter(
-        alert => alert.system_wide_flag !== true
-      );
-
-      const nonSystemAlertsSorted = nonSystemAlerts.sort((a, b) => {
-        return b.begin - a.begin;
-      });
-
-      setAlertsData(nonSystemAlertsSorted);
-      setSystemAlertsData(systemAlerts);
-
-      // console.log(result);
-    });
+    getSytemAlerts().then(result =>
+      processAlertsData(result, setAlertsData, setSystemAlertsData)
+    );
   }, []);
 
   return (
