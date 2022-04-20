f745e0fd5c9b7b6f24592d23d7a30b7c8e079dd7
wintermuted <jameson.nyeholt@gmail.com>
2022-04-19 20:58:09 -0700

    feat(stops): show alerts in stop location view

--- src/view/stop/component/StopLocationView.tsx
+++ src/view/stop/component/StopLocationView.tsx
@@ -1,10 +1,16 @@
 import { sortBy, uniq } from "lodash";
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import {
+  getAlertsByLocationId,
+  getAlertsByRouteId
+} from "../../../api/trimet/alerts";
 import { getArrivals } from "../../../api/trimet/arrivals";
+import { Alert, AlertsData } from "../../../api/trimet/interfaces/alertsData";
 import { ArrivalData } from "../../../api/trimet/interfaces/arrivals";
 import { StopLocation } from "../../../api/trimet/interfaces/types";
 import Map, { LatLngCoords } from "../../../component/map/Map";
+import { AlertsCard } from "../../lineDetail/component/AlertsCard";
 import CollapsiblePane from "../../lineDetail/component/CollapsiblePane";
 import Loading from "../../loading/Loading";
 import StopLocationArrivals from "./StopLocationArrivals";
@@ -23,14 +29,36 @@ interface Props {
   locationId: number;
 }
 
+interface MapCardParams {
+  coordinates: number[];
+}
+
+function MapCard({ coordinates }: MapCardParams) {
+  return (
+    <Card>
+      <Card.Header as="h5">Map</Card.Header>
+      <Card.Body>
+        <Card.Text>
+          <Map currentLocation={coordinates} />
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
 export default function StopLocationView(props: Props) {
   const { onBookmarkClick, stopIsBookmarked, locationId } = props;
   const [arrivalData, setArrivalData] = useState<ArrivalData>(undefined);
+  const [alertsData, setAlertsData] = useState<Alert[]>(undefined);
 
   useEffect(() => {
     const locIDs = locationId.toString(10);
     getArrivals(locIDs, 45).then(results => {
       setArrivalData(results);
+    });
+
+    getAlertsByLocationId(locationId).then((result: AlertsData) => {
+      setAlertsData(result.alert);
     });
   }, []);
 
@@ -56,28 +84,20 @@ export default function StopLocationView(props: Props) {
       </Row>
       <br />
       <Row>
-        <Col>
+        <Col md="3">
           <LocationInfoPane arrivalData={arrivalData} />
+          <br />
+          <AlertsCard alertsData={alertsData} />
         </Col>
-        <Col>
-          <CollapsiblePane className={undefined} title={"Map"} open={true}>
-            <Map currentLocation={coordinates} />
-          </CollapsiblePane>
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col>
+        <Col md="9">
           <StopLocationArrivalsTableNav
             routes={routes}
             locationId={location.id}
           />
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col>
+          <br />
           <StopLocationArrivals arrivalData={arrivalData} />
+          <br />
+          <MapCard coordinates={coordinates} />
         </Col>
       </Row>
     </Container>
