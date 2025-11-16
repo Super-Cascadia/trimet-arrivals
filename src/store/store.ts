bc6cb6687e9649a0c2218f61b5ac625d3dc8c08a
wintermuted <jameson.nyeholt@gmail.com>
2025-11-15 17:27:43 -0800

    WIP

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import reducers from "./reducers";
 import { rootSaga } from "./sagas";
 
